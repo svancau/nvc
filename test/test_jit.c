441fbb1446155327facc5200e58aeb5f82f8cb14
Nick Gasson <nick@nickg.me.uk>
2018-06-23 13:32:48 +0800

    Add loop test case

--- test/test_jit.c
+++ test/test_jit.c
@@ -49,6 +49,43 @@ START_TEST(test_add1)
 }
 END_TEST
 
+START_TEST(test_loop)
+{
+   vcode_unit_t unit = emit_function(ident_new("loop"), context, vint32);
+   vcode_reg_t p1 = emit_param(vint32, vint32, ident_new("p1"));
+   vcode_reg_t ctr = emit_alloca(vint32, vint32, VCODE_INVALID_REG);
+   emit_store_indirect(emit_const(vint32, 0), ctr);
+
+   vcode_block_t testbb = emit_block();
+   vcode_block_t exitbb = emit_block();
+   vcode_block_t bodybb = emit_block();
+   emit_jump(testbb);
+
+   vcode_select_block(testbb);
+   vcode_reg_t loaded = emit_load_indirect(ctr);
+   vcode_reg_t test = emit_cmp(VCODE_CMP_EQ, loaded, emit_const(vint32, 10));
+   emit_cond(test, exitbb, bodybb);
+
+   vcode_select_block(bodybb);
+
+   vcode_reg_t r = emit_add(loaded, emit_const(vint32, 1));
+   emit_jump(testbb);
+
+   vcode_select_block(exitbb);
+   emit_return(loaded);
+
+   vcode_opt();
+   vcode_dump();
+
+   uint32_t (*fn)(int) = jit_vcode_unit(unit);
+   fail_if(fn == NULL);
+
+   uint32_t result = (*fn)(5);
+   printf("result=%d\n", result);
+   fail_unless(result == 6);
+}
+END_TEST
+
 Suite *get_jit_tests(void)
 {
    Suite *s = suite_create("jit");
@@ -56,6 +93,7 @@ Suite *get_jit_tests(void)
    TCase *tc = nvc_unit_test();
    tcase_add_test(tc, test_ret42);
    tcase_add_test(tc, test_add1);
+   tcase_add_test(tc, test_loop);
    tcase_add_checked_fixture(tc, setup, teardown);
    suite_add_tcase(s, tc);
 
