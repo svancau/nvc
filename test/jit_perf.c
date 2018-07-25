fa847de28e2049b55c527ba234df74922182785f
Nick Gasson <nick@nickg.me.uk>
2018-07-25 09:26:27 +0100

    Make jit_perf optional

--- test/jit_perf.c
+++ test/jit_perf.c
@@ -7,18 +7,30 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <time.h>
+#include <limits.h>
+#include <dlfcn.h>
+
+void _bounds_fail(void)
+{
+   assert(false);
+}
 
 static void test_fact(void)
 {
    extern int fact(int);
 
-   vcode_unit_t unit = vcode_find_unit(ident_new("WORK.JITPERF.FACT(N)N"));
+   vcode_unit_t unit =
+      vcode_find_unit(ident_new("JITPERFLIB.JITPERF.FACT(N)N"));
    assert(unit);
 
    uint32_t (*fn)(int) = jit_vcode_unit(unit);
    assert(fn);
 
+   uint32_t (*lfn)(int) = dlsym(NULL, "JITPERFLIB.JITPERF.FACT(N)N");
+   assert(lfn);
+
    assert((*fn)(4) == 24);
+   assert((*lfn)(4) == 24);
    assert(fact(4) == 24);
 
    uint64_t cstart = get_timestamp_us();
@@ -31,7 +43,13 @@ static void test_fact(void)
       (*fn)(i & 15);
    double vtime = (get_timestamp_us() - vstart) / 1000.0;
 
-   printf("Factorial   C: %.1f ms; VHDL: %1.f ms\n", ctime, vtime);
+   uint64_t lstart = get_timestamp_us();
+   for (int i = 0; i < 100000000; i++)
+      (*lfn)(i & 15);
+   double ltime = (get_timestamp_us() - lstart) / 1000.0;
+
+   printf("Factorial   C: %.1f ms; JIT: %.1f ms; LLVM: %.1f\n",
+          ctime, vtime, ltime);
 }
 
 int main(int argc, char **argv)
@@ -49,9 +67,11 @@ int main(int argc, char **argv)
    opt_set_str("dump-vcode", NULL);
    opt_set_int("ignore-time", 0);
    opt_set_int("verbose", 0);
+   opt_set_int("optimise", 2);
+   opt_set_int("dump-llvm", 0);
    intern_strings();
 
-   lib_t test = lib_tmp("work");
+   lib_t test = lib_new("JITPERFLIB", "jitperf");
    lib_set_work(test);
 
    set_standard(STD_93);
@@ -79,7 +99,17 @@ int main(int argc, char **argv)
 
    simplify(body, 0);
    bounds_check(body);
-   lower_unit(body);
+   vcode_unit_t unit = lower_unit(body);
+   cgen(body, unit);
+
+   char sopath[PATH_MAX];
+   lib_realpath(test, "_JITPERFLIB.JITPERF-body.so", sopath, sizeof(sopath));
+
+   void *dlh = dlopen(sopath, RTLD_GLOBAL | RTLD_NOW);
+   if (dlh == NULL) {
+      fprintf(stderr, "%s: %s\n", sopath, dlerror());
+      return 1;
+   }
 
    test_fact();
 
